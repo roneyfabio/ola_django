a5ce81dec533d9f525b2e45b6d0af25c1112a856
Roney Fábio <roneyfabio@gmail.com>
2026-05-19 09:40:13 -0300

    444

--- blog/urls.py
+++ blog/urls.py
@@ -21,7 +21,7 @@ app_name = 'blog'
 
 # blog/
 urlpatterns = [    
+    path('post/<int:id>/', views.post, name='post'),
     path('', views.blog, name='home'),
-    path('post/<int:id>', views.post, name='post'),
     path('exemplo/', views.exemplo, name='exemplo'),
 ]
